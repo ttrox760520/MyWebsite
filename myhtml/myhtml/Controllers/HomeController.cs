4368dbdc24357dc949a027bbd7c24f55efd8bb03
ttrox760520 <ttrox760520@gmail.com>
2019-09-10 16:54:48 +0800

    第一版

---
+++ myhtml/myhtml/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myhtml.Models.Home;
+
+namespace myhtml.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IndexModel model1 = new IndexModel();
+            model1.text = "測試~測試~";
+            return View(model1);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
